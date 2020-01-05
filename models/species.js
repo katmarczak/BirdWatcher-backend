9aa26a10877553e74f6548e901e2deda043a1fe2
Katarzyna Nęcka <katarzyna.necka@gmail.com>
2020-01-05 14:57:15 +0100

    add tests for species model

--- models/species.js
+++ models/species.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Joi = require('joi');
 
 const speciesSchema = new mongoose.Schema({
     commonName: { type: String, required: true },
@@ -9,5 +10,27 @@ const speciesSchema = new mongoose.Schema({
 
 const Species = mongoose.model('Species', speciesSchema);
 
+function validateSpecies(species) {
+    const schema = {
+        commonName: Joi.string()
+            .min(3)
+            .max(255)
+            .required(),
+        scientificName: Joi.string()
+            .min(3)
+            .max(255)
+            .required(),
+        statusPL: Joi.string()
+            .min(1)
+            .max(3),
+        AERCcategoryPL: Joi.string()
+            .length(1)
+            .uppercase()
+    };
+
+    return Joi.validate(species, schema);
+}
+
 module.exports.speciesSchema = speciesSchema;
 module.exports.Species = Species;
+module.exports.validateSpecies = validateSpecies;
