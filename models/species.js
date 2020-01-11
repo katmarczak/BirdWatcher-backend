fc0967463babf29fbbb8e523e225b3cd8b122fa8
Katarzyna Nęcka <katarzyna.necka@gmail.com>
2020-01-11 21:32:59 +0100

    fix for travis not finding joi

--- models/species.js
+++ models/species.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Joi = require('joi');
+const Joi = require('@hapi/joi');
 
 const speciesSchema = new mongoose.Schema({
     commonName: { type: String, required: true },
